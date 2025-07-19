566515888f33af489304543b57733b1d3a82e87f
andrews <andrewsdrums250@gmail.com>
2025-07-19 11:36:07 -0500

    Forzar nuevo deploy tras cancelar workflow atascado

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BEATOREACT2/', // Configuración correcta para GitHub Pages
   plugins: [react()],
-  base: '/BEATOREACT2/', // ¡Añade esta línea!
-})
+});
